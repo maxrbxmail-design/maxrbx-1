be50863968b72c37eca182da420d65af9cfbe63e
maxrbxmail-design <maxrbxmail@gmail.com>
2026-04-01 00:06:10 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
+import Image from 'next/image';
 
 export default function HomePage() {
   const [username, setUsername] = useState('');
