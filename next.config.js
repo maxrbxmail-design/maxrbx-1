2a3368014b378f0446e6fe081c5a8830b7104a2a
maxrbxmail-design <maxrbxmail@gmail.com>
2026-03-31 19:41:23 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'tr.rbxcdn.com',   // Roblox avatar CDN
+      },
+      {
+        protocol: 'https',
+        hostname: '**.rbxcdn.com',   // wildcard for all Roblox CDN subdomains
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
